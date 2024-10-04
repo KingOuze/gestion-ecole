63cb5a1f34b598f1042a83f6655a5b18e531e100
YayeFatou07 <kaneyafatou20@gmail.com>
2024-10-04 17:30:20 +0000

    j'ai travaillé sur la modification des données de l'administrateur

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$host = 'localhost'; // Adresse de l'hôte (généralement localhost)
+$dbname = 'gestion-ecole'; // Nom de votre base de données
+$user = 'root'; // Nom d'utilisateur de la base de données
+$password = ''; // Mot de passe de la base de données
+try {
+    // Créer la connexion avec PDO
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    // Définir le mode d'erreur de PDO sur Exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Vous pouvez ajouter d'autres attributs selon vos besoins
+} catch (PDOException $e) {
+    // Gestion des erreurs de connexion
+    die("Erreur de connexion : " . $e->getMessage());
+}
