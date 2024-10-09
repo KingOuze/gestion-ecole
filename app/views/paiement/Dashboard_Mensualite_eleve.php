047b3889c97cb308a5e53fc44f9fed6536a9257c
NdiongueFH <hawa.ndiongue@gmail.com>
2024-10-09 09:07:02 +0000

    travail sur la mensualite des eleves

---
+++ app/views/paiement/Dashboard_Mensualite_eleve.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des Paiements des Élèves</title>
+    <link rel="stylesheet" href="mensualite.css">
+
+</head>
+<body>
+
+    <div class="sidebar">
+            <div class="logo">
+                <img src="Badge_Education_Badge_Logo.png" alt="Logo">
+                <h2 class="school-name">Ecole de la réussite</h2>
+            </div>
+            <ul>
+                <li><a href="#">Tableau de Bord</a></li>
+                <li><a href="#">Gestion Finance</a></li>
+            </ul>
+    </div>
+
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center py-3">
+            <p class="centered-paragraph">Gestion du paiement des élèves</p>
+            <a href="#" class="deconnexion-link">
+                <span>Déconnexion</span>
+                <img src="iconeDeconnexion.png" alt="Déconnexion" class="deconnexion-icon">
+            </a>
+        </div>
+        <div class="statistic-container">
+            <div class="stat">
+                <h2>5,423</h2>
+                <p>Total Élève</p>
+            </div>
+            <div class="divider"></div>
+            <div class="stat">
+                <h2>1,893</h2>
+                <p>Total Élève Payé</p>
+            </div>
+            <div class="divider"></div>
+            <div class="stat">
+                <h2>189</h2>
+                <p>Total Élève Non Payé</p>
+            </div>
+        </div>
+
+        <div class="search">
+            <input type="text" placeholder="Recherche par matricule">
+            <button class="search-button">
+                <span class="icon">🔍</span>
+            </button>
+        </div>
+
+        <div class="image-container">
+            <img src="imagePaiement.png" alt="Illustration" style="max-width: 100%; height: auto;">
+        </div>
+        
+   </div>
+
+</body>
+</html>
---
+++ app/views/paiement/lister_eleve.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des Paiements des Élèves</title>
+    <link rel="stylesheet" href="lister.css">
+
+</head>
+<body>
+
+    <div class="sidebar">
+            <div class="logo">
+                <img src="Badge_Education_Badge_Logo.png" alt="Logo">
+                <h2 class="school-name">Ecole de la réussite</h2>
+            </div>
+            <ul>
+                <li><a href="#">Tableau de Bord</a></li>
+                <li><a href="#">Gestion Finance</a></li>
+            </ul>
+    </div>
+
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center py-3">
+            <p class="centered-paragraph">Paiement Mensualite</p>
+            <div class="search">
+                <input type="text" placeholder="Recherche par matricule">
+                <button class="search-button1">
+                    <span class="icon">🔍</span>
+                </button>
+            </div>
+            <a href="#" class="deconnexion-link">
+                <span>Déconnexion</span>
+                <img src="iconeDeconnexion.png" alt="Déconnexion" class="deconnexion-icon">
+            </a>
+            
+
+        </div>
+       
+
+        
+      
+        
+   </div>
+
+</body>
+</html>
