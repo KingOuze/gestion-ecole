813102cb4da2e33d2e602ceb3ec727a7196d9a97
YayeFatou07 <kaneyafatou20@gmail.com>
2024-10-15 10:25:09 +0000

    Réinitialisation

--- public/js/modifier.js
+++
@@ -1,5 +0,0 @@
-function confirmDelete(matricule) {
-    if (confirm("Êtes-vous sûr de vouloir supprimer cette soumission ?")) {
-        window.location.href = 'supprimer.php?matricule=' + matricule;
-    }
-}
--- public/js/suppression.js
+++
@@ -1,5 +0,0 @@
-function confirmArchive(matricule) {
-    if (confirm("Êtes-vous sûr de vouloir archiver cet administrateur ?")) {
-        window.location.href = 'supprimer.php?matricule=' + matricule;
-    }
-}
